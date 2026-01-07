777cd092dba75e5150c401a725bf16186fed65f4
Keylime-P1 <cpleonard2002@gmail.com>
2026-01-07 16:55:08 -0700

    Wrote the program to ask user for a grade percentage, then assign a grade and declare whether you pass or fail

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string gradePercent = Console.ReadLine();
+        int gradePercentInt = int.Parse(gradePercent);
+        if (gradePercentInt >= 90)
+        {
+            Console.Write("You got an A");
+        }
+        else if (gradePercentInt >= 80)
+        {
+            Console.Write("You got a B");
+        }
+        else if (gradePercentInt >= 70)
+        {
+            Console.Write("You got a C");
+        }
+        else if (gradePercentInt >= 60)
+        {
+            Console.Write("You got a D");
+        }
+        else
+        {
+            Console.Write("You got an F");
+        }
+
+        if (gradePercentInt >= 70)
+        {
+            Console.WriteLine(", You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine(", You failed the class");
+        }
+        Console.WriteLine();
     }
 }
