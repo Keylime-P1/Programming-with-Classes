1bb0442ae32d84306df35c657f7cbd5c2fbda43e
Keylime-P1 <cpleonard2002@gmail.com>
2026-01-09 12:39:49 -0700

    Created number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guessCount = 1;
+        Random randomGenerator = new Random();
+        int luckyNumber = randomGenerator.Next(1,101);
+
+        Console.WriteLine("Welcome the the Number guessing game!");
+        string userGuess = "";
+        string playGame = "";
+        
+
+        while (playGame != "no")
+        {
+            Console.WriteLine("What is your guess?");
+            userGuess = Console.ReadLine();
+
+            // int luckyNumParse = int.Parse(luckyNumber);
+            int userGuessParse = int.Parse(userGuess);
+
+            if (userGuessParse > luckyNumber)
+            {
+                Console.WriteLine("Lower");
+                Console.WriteLine("");
+            }
+            else if (userGuessParse < luckyNumber)
+            {
+                Console.WriteLine("Higher");
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("You guessed the lucky number!");
+                Console.WriteLine("");
+                Console.WriteLine($"You guessed {guessCount} times.");
+                Console.WriteLine("Do you want to play again? (yes/no)");
+                playGame = Console.ReadLine();
+                if (playGame == "yes")
+                {
+                    guessCount = 0;
+                    luckyNumber = randomGenerator.Next(1,101);
+                }
+            }
+            guessCount++;
+        }
+        Console.WriteLine("Thanks for playing!");
+        Console.WriteLine("");
     }
 }
