1d4f46465f6940ae87b1a0abc3400b59f6c553ff
Keylime-P1 <cpleonard2002@gmail.com>
2026-01-10 09:23:07 -0700

    Created the program per the instructions to take a list of numbers and return the sum of average of the numbers given

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,35 @@
-using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        string userNumString;
+        int userNum = 1;
+        int numTotal = 0;
+        int numAverage = 0;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (userNum != 0)
+        {
+            Console.Write("Enter a number: ");
+            userNumString = Console.ReadLine();
+            userNum = int.Parse(userNumString);
+            if (userNum  !=0)
+            {
+                numbers.Add(userNum);
+            }
+            else
+            {
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    numTotal = numTotal + numbers[i];
+                }
+                numAverage = numTotal / numbers.Count;
+                Console.WriteLine($"Your sum is {numTotal}");
+                Console.WriteLine($"Your average is {numAverage}");
+            }
+        }
     }
 }
