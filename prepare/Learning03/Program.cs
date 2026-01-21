8e443722fd759342488eddf8470e14d4281c2186
Keylime-P1 <cpleonard2002@gmail.com>
2026-01-21 16:13:38 -0700

    Created program to access fraction class. created Fraction class

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction myFraction1 = new Fraction();
+        Fraction myFraction2 = new Fraction(4);
+        Fraction myFraction3 = new Fraction(2,3);
+        
+        Console.WriteLine("Initialized values:");
+        Console.WriteLine($"{myFraction1.GetTop()}, {myFraction2.GetTop()}, {myFraction3.GetTop()} ");
+        Console.WriteLine($"{myFraction1.GetBottom()}, {myFraction2.GetBottom()}, {myFraction3.GetBottom()} ");
+        
+        Console.Write("\nSet new top value:");
+        string newTopString = Console.ReadLine();
+        int newTop = int.Parse(newTopString);
+        myFraction1.SetTop(newTop);
+
+        Console.Write("Set new bottom value:");
+        string newBottomString = Console.ReadLine();
+        int newBottom = int.Parse(newBottomString);
+        myFraction1.SetBottom(newBottom);
+
+        Console.WriteLine($"\nNew top value: {myFraction1.GetTop()}\nNew bottom value: {myFraction1.GetBottom()}");
+        double mydecimal; 
+
+        Console.WriteLine("fraction methods test");
+        Console.WriteLine(myFraction1.GetFractionString());
+        mydecimal = myFraction1.GetDecimalValue();
+        Console.WriteLine(mydecimal);
+        
     }
 }
